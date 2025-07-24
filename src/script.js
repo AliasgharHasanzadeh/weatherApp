19a1780d7ad5acda1d7ecd98702f7f502ff2323b
AliasgharHasanzadeh <a.hasanzadeh2006@gmail.com>
2025-07-24 14:59:01 +0330

    update search part

--- src/script.js
+++ src/script.js
@@ -6,54 +6,62 @@
 let data = null;
 async function fetchWeather(city) {
   loading.style.display = "block";
-  const res = await fetch(url + city );
+  const res = await fetch(url + city);
   data = await res.json();
   // console.log(data)
   loading.style.display = "none";
-  setHeader()
+  setHeader();
   // daily forecast
- setForcastBus(0)
-//  weekly forecast set
+  setForcastBus(0);
+  //  weekly forecast set
   weeklyForecastEl.forEach((item, index) => {
-
-
-
     item.addEventListener("click", function () {
-      weeklyForecastEl.forEach(val=>{
-        val.classList.remove('active')
-      })
-      item.classList.add('active')
+      weeklyForecastEl.forEach((val) => {
+        val.classList.remove("active");
+      });
+      item.classList.add("active");
       const day = item.getAttribute("data-day");
-      setForcastBus(day)
+      setForcastBus(day);
     });
   });
 }
 fetchWeather("london");
+
 btnSearch.addEventListener("click", function () {
   const temp = loc.value;
   fetchWeather(temp);
 });
+window.addEventListener("keypress", function(e){
+  if ((e.key == "Enter")) {
+    const temp = loc.value;
+    fetchWeather(temp);
+  }
+});
 
-function setForcastBus(day){
-  dayForcastEl.innerHTML = data.forecast.forecastday[day].date.replaceAll("-", "/") + " Forecast";
+function setForcastBus(day) {
+  dayForcastEl.innerHTML =
+    data.forecast.forecastday[day].date.replaceAll("-", "/") + " Forecast";
   forecastBoxEl.forEach((item, index) => {
     const time = index * 2;
     item.innerText = data.forecast.forecastday[day].hour[time].temp_c + " °C";
   });
   forecastBoxIConEl.forEach((item, index) => {
-    console.log(`${index*2}AM:`,statusWeather(
-      data.forecast.forecastday[day].hour[index*2].condition.code,
-      data.forecast.forecastday[day].hour[index*2].is_day
-    ))
+    console.log(
+      `${index * 2}AM:`,
+      statusWeather(
+        data.forecast.forecastday[day].hour[index * 2].condition.code,
+        data.forecast.forecastday[day].hour[index * 2].is_day
+      )
+    );
     item.innerHTML = giveWeatherICon(
       statusWeather(
-        data.forecast.forecastday[day].hour[index*2].condition.code,
-        data.forecast.forecastday[day].hour[index*2].is_day
+        data.forecast.forecastday[day].hour[index * 2].condition.code,
+        data.forecast.forecastday[day].hour[index * 2].is_day
       )
     );
   });
 }
-function setHeader(){
+function setHeader() {
   locEl.innerText = data.location.country + "," + data.location.name;
   tempEl.innerText = data.current.temp_c;
   feelsLike.innerText = "Feels " + data.current.feelslike_c + " °C";
@@ -61,10 +69,9 @@ function setHeader(){
     statusWeather(data.current.condition.code, data.current.is_day)
   );
   WeatherStatusEl.innerText = toTitleCase(
-    statusWeather(data.current.condition.code, data.current.is_day).replace(
-      "_",
-      " "
-    ).replace('_',' ')
+    statusWeather(data.current.condition.code, data.current.is_day)
+      .replace("_", " ")
+      .replace("_", " ")
   );
   precipEl.innerHTML =
     precipSvg + "Precipitation : " + data.current.precip_mm + " mm";
